2293176f0d3e4aa8bd1908592bfdad3ecd4e202f
cucopc <37154421+cucopc@users.noreply.github.com>
2018-03-08 16:27:37 +0100

    Update grabar.sh

--- grabar.sh
+++ grabar.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 arecord -D plughw:1,0 -q -f cd -t wav -d 4 -r 16000 voz.wav -c 1
 
+#Llamamos al script y pasamos todo a minusculas para no terner problemas
 python wit.py | tr 'A-Z' 'a-z' > voz.txt;
 
 value=`cat voz.txt`
